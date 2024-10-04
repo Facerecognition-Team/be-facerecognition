f53c729161305c1c106f01eba2351e33e4c4ab99
Hamzah . <hamzahibnabdullah@gmail.com>
2024-10-04 17:51:02 +0700

    add flow di api send image

--- app/Http/Controllers/FaceRecognitionController.php
+++ app/Http/Controllers/FaceRecognitionController.php
@@ -9,27 +9,34 @@ class FaceRecognitionController extends Controller
 {
     public function sendImage(Request $request)
     {
+        // Gunakan 2 tabel, absen, dan pegawai
+
         // Validasi bahwa permintaan berisi file gambar
         $request->validate([
             'image' => 'required|image|max:10240', // Ukuran maksimal 10MB
+            'id' => 'required|integer',
         ]);
 
         // Ambil gambar yang diunggah
         $image = $request->file('image');
+        $id = $request->input('id');
+        return $id;
 
-        // Konversi gambar ke base64
-        $imageBase64 = base64_encode(file_get_contents($image));
+        // Simpan gambar ke server, lalu ambil path-nya
 
-        // Kirim permintaan POST ke server Flask dengan gambar
-        $response = Http::post('http://127.0.0.1:5000/recognize', [
-            'image' => $imageBase64,
-        ]);
+        // Jika sukses simpan gambar ke server, dilanjut simpan ke tabel absen path, id, jam absen (pakai get time di php) ke database
+
+        // Jika gagal simpan gambar, return error
+
+        // Jika sukses simpan data, kembalikan respon id, nama dari tabel pegawai dan jam absen
+
+        // Jika gagal, kembalikan pesan error dan hapus gambar dari server
 
         // Cek apakah respons dari Flask berhasil
-        if ($response->successful()) {
-            return response()->json($response->json());
-        } else {
-            return response()->json(['error' => 'Failed to process the image'], $response->status());
-        }
+        // if ($response->successful()) {
+        //     return response()->json($response->json());
+        // } else {
+        //     return response()->json(['error' => 'Failed to process the image'], $response->status());
+        // }
     }
 }
